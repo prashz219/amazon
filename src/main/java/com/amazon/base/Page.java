1ac294a9c5a7a04905198c1a0973c9b69e8742d6
prashz219 <prashz219@gmail.com>
2019-03-15 13:17:30 +0530

    Added files to page class and some dependencies

---
+++ src/main/java/com/amazon/base/Page.java
@@ -0,0 +1,85 @@
+package com.amazon.base;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class Page {
+
+	/*
+	 * Initializing WebDriver ExcelReader Logs WebDriverWait ExtentReports
+	 * 
+	 * 
+	 * 
+	 */
+
+	public static WebDriver driver;
+	public static Properties config = new Properties();
+	public static Properties OR = new Properties();
+	public static FileInputStream fis;
+	public static WebDriverWait wait;
+	
+	@BeforeSuite
+	public void setUp() {
+
+
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\src\\test\\resources\\executables\\chromedriver.exe");
+		driver = new ChromeDriver();
+		
+		try {
+			fis = new FileInputStream(
+					System.getProperty("user.dir") + "\\src\\test\\resources\\properties\\Config.properties");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			config.load(fis);
+			//log.debug("Config file loaded !!!");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			fis = new FileInputStream(
+					System.getProperty("user.dir") + "\\src\\test\\resources\\properties\\OR.properties");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			OR.load(fis);
+			//log.debug("OR file loaded !!!");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		driver.get(config.getProperty("testsiteurl"));
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		
+		
+		}
+
+	
+
+	@AfterSuite
+	public void tearDown() {
+		
+		driver.quit();
+
+	}
+
+}
---
+++ src/test/java/com/amazon/rough/TestClass.java
@@ -0,0 +1,19 @@
+package com.amazon.rough;
+
+import org.testng.annotations.Test;
+
+import com.amazon.base.Page;
+
+public class TestClass extends Page{
+
+		
+		@Test
+		public void testClass (){
+			
+			System.out.println("Success!!");
+			
+		}
+		
+	}
+
+
