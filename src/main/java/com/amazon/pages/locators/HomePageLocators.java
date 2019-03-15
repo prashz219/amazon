912c6b3de19e4233bf571c0ff76e37ec88e21473
prashz219 <prashz219@gmail.com>
2019-03-15 22:54:30 +0530

    implemented browser settings

--- src/main/java/com/amazon/pages/locators/HomePageLocators.java
+++ src/main/java/com/amazon/pages/locators/HomePageLocators.java
@@ -1,16 +1,18 @@
 package com.amazon.pages.locators;
 
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
 
 
 
 public class HomePageLocators {
-	
-	
-	
+		
 	//locators of HomePage
-	@FindBy(css="#nav-link-yourAccount [class='nav-line-2']")
+	@FindAll({
+		@FindBy(css="#nav-link-yourAccount [class='nav-line-2']"),
+		@FindBy(xpath="//*[contains(text(),'Hello, Sign in')]")
+	})
 	public WebElement HomePageHelloSignin;
 	
 
